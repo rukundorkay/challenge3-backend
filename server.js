fa56dc1d1a6070a66af26f0d7cf5b713071a993d
rukundorkay <rukundojanvier250@gmail.com>
2021-07-20 13:07:14 +0200

    i added new staf

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ import express from 'express';
 import Router from './routes/index.js';
 const app = express();
 app.use("/", Router);
-const port = process.env.port || 3000;
-app.listen(port, console.log(`server is runnning on server of ${port}`))
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, console.log(`server is runnning on server of ${PORT}`))
