8547359aeda842dfc23a0044c5ab0978f35412f3
rukundorkay <rukundojanvier250@gmail.com>
2021-07-20 12:58:19 +0200

    change port to 3000

--- server.js
+++ server.js
@@ -2,5 +2,5 @@ import express from 'express';
 import Router from './routes/index.js';
 const app = express();
 app.use("/", Router);
-const port = process.env.port || 5000;
+const port = process.env.port || 3000;
 app.listen(port, console.log(`server is runnning on server of ${port}`))
