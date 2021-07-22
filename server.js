b42010556bdb851d630e3e8e63ec2c5789524dd2
rukundorkay <rukundojanvier250@gmail.com>
2021-07-22 09:54:44 +0200

    i added  code to allow cross orgin request

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 import express from 'express';
 import Router from './routes/index.js';
 const app = express();
+// app.use(express.static(__dirname))
+app.use((req, res, next) => {
+    res.set('Access-Control-Allow-Origin', '*')
+    next()
+})
 app.use("/", Router);
+
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, console.log(`server is runnning on server of ${PORT}`))
