877e69760471844441c0a55b9ef965c76ef4335b
rukundorkay <rukundojanvier250@gmail.com>
2021-07-22 13:31:34 +0200

    i made some changes

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,7 @@ const Router = express();
 Router.get('/', (req, res) => {
     res.status(200);
     res.setHeader('Content-Type', 'text/html;charset=utf8');
-    res.end(`<h1>WELCOME TO Challenge3 API</h1>\n Enjoy using our API for challenge 3\n  to click <a href="https://vast-eyrie-17161.herokuapp.com">here </a>  to explore challeng3  `);
+    res.end(`<h1>WELCOME TO Challenge3 API</h1>\n Enjoy using our API for challenge 3  click <a href="https://vast-eyrie-17161.herokuapp.com">here </a>  for live app `);
 });
 
 Router.get('/album/:id', checkAlbumAvailabity);
